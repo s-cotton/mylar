58c52f0d93db5e723a9d8b0eb9cb8399cbc19abd
StephenJCotton <cot.s.ton@gmail.com>
2017-02-21 08:28:27 -0500

    Merge of standalone comic composite image functionality into mylar

--- mylar/helpers.py
+++ mylar/helpers.py
@@ -23,6 +23,12 @@ import shutil
 import os, errno
 import mylar
 import logger
+import sys
+from PIL import Image
+import StringIO
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def multikeysort(items, columns):
 
@@ -1372,8 +1378,21 @@ def filesafe(comic):
 
 
 def IssueCovers(ComicID=None):
-    if mylar.COVER_VIEW:
-        import zipfile, logger, pillow
+    if mylar.ENABLE_COVER_COMPOSITION:
+        import logger
+        import zipfile
+        if mylar.ENABLE_RAR:
+            import rarfile
+
+        # Acceptible image extensions to process
+        pic_extensions = ('.jpg','.png','.webp','.gif','.jpeg')
+
+        # Regexes to help with identifying Covers
+        numcheck = re.compile('[0-9]{2,3}')
+        onecheck = re.compile('0{1,2}1')
+        altcheck = re.compile('0{2,3}[A-Z]?[. ]', re.I)
+        merged_quality='web_medium'
+
         if ComicID is None:
             logger.fdebug('No Comic provided for Issue Cover Extraction')
             return
@@ -1383,147 +1402,142 @@ def IssueCovers(ComicID=None):
                 logger.fdebug('Invalid Comic passed for Issue Cover Extraction');
                 return
             else :
+                ComicPath = comic.ComicLocation
                 coverfiles = []
                 issues = myDB.select('SELECT * FROM issues WHERE ComicID=? order by Int_IssueNumber DESC', [ComicID])
                 for issue in issues:
-                    iszip = false
-                    if issue['Location'].endswith('.cbz'):
-                        iszip = true
-                        dstlocation = os.path.join(mylar.CACHE_DIR, 'imgtemp.zip')
-                    else :
-                        dstlocation = os.path.join(mylar.CACHE_DIR, 'imgtemp.rar')
-
-                    shutil.copy( issue['Location'], dstlocation );
-
-                    issuecovers = []
-
-                    cover = "notfound"
+                    rawfilename = issue.Location
+                    logger.fdebug("Processing " + rawfilename + '...')
+                    # locate the file in the filesystem
+                    srclocation = os.path.join( ComicPath, rawfilename )
+                    # track total covers for temp file naming
                     covercount = 0
-                    low_infile = 999999
+                    # Temp Storage for found cover images
+                    cover_filenames = []
+                    # filtered storage for cover images (remove fallback 001 if we have more than one found cover)
+                    final_cover_filenames = []
+                    loaded_archive = False
 
-                    if iszip:
-                        try:
-                            with zipfile.ZipFile(dstlocation, 'r') as issuefile:
-                                
-                                for infile in issuefile.namelist():
-                                    tmp_infile = re.sub("[^0-9]","", infile).strip()
-                                    if tmp_infile == '':
-                                        pass
-                                    elif int(tmp_infile) < int(low_infile):
-                                        low_infile = tmp_infile
-                                        low_infile_name = infile
-
-                                    if any(['000.' in infile, '00.' in infile]) and infile.endswith(pic_extensions):
-                                        logger.fdebug('Extracting primary image ' + infile + ' as coverfile for display.')
-                                        local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                        local_file.write(inzipfile.read(infile))
-                                        local_file.close
-                                        cover = "found"
-                                        covercount += 1
-                                    elif any(['00a' in infile, '00b' in infile, '00c' in infile, '00d' in infile, '00e' in infile]) and infile.endswith(pic_extensions):
-                                        logger.fdebug('Found Alternate cover - ' + infile + ' . Extracting.')
-                                        altlist = ('00a', '00b', '00c', '00d', '00e')
-                                        for alt in altlist:
-                                            if alt in infile:
-                                                local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                                local_file.write(inzipfile.read(infile))
-                                                local_file.close
-                                                cover = "found"
-                                                covercount += 1
-                                                break
-
-                                    elif any(['001.jpg' in infile, '001.png' in infile, '001.webp' in infile, '01.jpg' in infile, '01.png' in infile, '01.webp' in infile]):
-                                        logger.fdebug('Extracting primary image ' + infile + ' as coverfile for display.')
-                                        local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                        local_file.write(inzipfile.read(infile))
-                                        local_file.close
-                                        cover = "found"
-                                        covercount += 1
-
-                                if cover != "found":
-                                    logger.fdebug('Invalid naming sequence for jpgs discovered. Attempting to find the lowest sequence and will use as cover (it might not work). Currently : ' + str(low_infile))
-                                    local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                    local_file.write(inzipfile.read(low_infile_name))
-                                    local_file.close
-                                    cover = "found"                
-
-                        except:
-                            logger.info('ERROR. Could not open ' + issue['Location'] + ' for cover extraction')
-                        return
-                    else:
-                        if mylar.ENABLE_RAR:
-                            import rarfile
-                            
-                            try:
-                                with rarfile.RarFile(dstlocation, 'r') as issuefile:
-                                    for inifile in issuefile.infolist():
-                                        tmp_infile = re.sub("[^0-9]","", infile.filename).strip()
-                                        if tmp_infile == '':
-                                            pass
-                                        elif int(tmp_infile) < int(low_infile):
-                                            low_infile = tmp_infile
-                                            low_infile_name = infile
-
-                                        if any(['000.' in infile, '00.' in infile]) and infile.endswith(pic_extensions):
-                                            logger.fdebug('Extracting primary image ' + infile + ' as coverfile for display.')
-                                            local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                            local_file.write(inzipfile.read(infile))
-                                            local_file.close
-                                            cover = "found"
-                                            covercount += 1
-                                        elif any(['00a' in infile, '00b' in infile, '00c' in infile, '00d' in infile, '00e' in infile]) and infile.endswith(pic_extensions):
-                                            logger.fdebug('Found Alternate cover - ' + infile + ' . Extracting.')
-                                            altlist = ('00a', '00b', '00c', '00d', '00e')
-                                            for alt in altlist:
-                                                if alt in infile:
-                                                    local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                                    local_file.write(inzipfile.read(infile))
-                                                    local_file.close
-                                                    cover = "found"
-                                                    covercount += 1
-                                                    break
-
-                                        elif any(['001.jpg' in infile, '001.png' in infile, '001.webp' in infile, '01.jpg' in infile, '01.png' in infile, '01.webp' in infile]):
-                                            logger.fdebug('Extracting primary image ' + infile + ' as coverfile for display.')
-                                            local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                            local_file.write(inzipfile.read(infile))
-                                            local_file.close
-                                            cover = "found"
-                                            covercount += 1
-
-                                    if cover != "found":
-                                        logger.fdebug('Invalid naming sequence for jpgs discovered. Attempting to find the lowest sequence and will use as cover (it might not work). Currently : ' + str(low_infile))
-                                        local_file = open(os.path.join(mylar.CACHE_DIR, 'temp-cover-' + covercount + '.jpg'), "wb")
-                                        local_file.write(inzipfile.read(low_infile_name))
-                                        local_file.close
-                                        cover = "found"                
-
-                            except:
-                                logger.info('ERROR. Could not open ' + issue['Location'] + ' for cover extraction')
+                    try:
+                        if rawfilename.endswith('.cbz'):
+                            # Load up the zip
+                            issuefile = zipfile.ZipFile(srclocation)
                         else :
-                            logger.fdebug("RAR File Processing not enabled")
+                            if mylar.ENABLE_RAR:
+                                # Load up the rar
+                                issuefile = rarfile.RarFile(srclocation)
+                            else :
+                                logger.fdebug('RAR File Processing is not Enabled, skipping issue...')
+                                continue
+                        loaded_archive = True
+                    except:
+                        logger.fdebug('ERROR. Could not open ' + rawfilename + ' for cover extraction: ' + str(sys.exc_info()))
 
-                    if cover != "notfound":
-                        logger.fdebug("No Covers found for this issue?")
-                        nocovers = []
-                        coverfile.append( nocovers )
-                    else :
-                        for coveroffset in range(0, covercount):
-                            issuecovers.append( os.path.join(mylar.CACHE_DIR, 'temp-cover-' + coveroffset + '.jpg') )
-                        coverfile.append( issuecovers )
+                    if loaded_archive:
+                        # raw, all images in archive
+                        allimages = []
+                        # all images that have a page number pattern
+                        allimagesclean = []
+                        # Determine common part of filenames to remove possible number based names that wouold interfere with determining cover files
+                        for afile in issuefile.namelist():
+                            if afile.endswith(pic_extensions):
+                                #print afile
+                                allimages.append(afile)
 
-                maxIssues = len(coverfile)
-                maxCovers = 0;
+                        # No need to re-invent the wheel
+                        common_name = os.path.commonprefix(allimages)
 
-                for singlecoverset in coverlist:
-                    if len(singlecoverset) > maxCovers:
-                        maxCovers = len(singlecoverset)
+                        # Loop over images and determine if they have page numbers, hopefully stripping out the scene files
+                        for afile in allimages:
+                            afile_trimmed = afile.replace(common_name,"")
+                            #print "Checking ", afile_trimmed, numcheck.match(afile_trimmed)
+                            if numcheck.search(afile_trimmed) is not None:
+                                allimagesclean.append(afile)
 
-                for singlecoverset in coverfile:
-                    logger.fdebug("Adding covers for single issue")
+                        common_name = os.path.commonprefix(allimagesclean)
+
+                        # Loop over all "valid" page number archived images
+                        for infile in allimagesclean:
+                            check_filename = infile.replace(common_name,"")
+                            filename,file_extension = os.path.splitext(check_filename)
+                            
+                            isCover = False
+                            
+                            # If our stripped down page number can be interpreted as a number, eval it as an integer (quickest way to get rid of 100 101 200 201 etc matches)
+                            if filename.isdigit():
+                                if int(filename) == 0 or int(filename) == 1:
+                                    isCover = True
+                            # It's not a number, so look specifically for 2-3 zeros followed by a possible letter.  This would still fail to restrict to covers if you have more than 1000 pages, but any real number should
+                            # be caught by the isidigit call above
+                            else:
+                                if altcheck.search(check_filename) is not None:
+                                    isCover = True
+
+                            if isCover:
+                                cover_filenames.append(infile)
+
+                        logger.fdebug("Found " + str(len(cover_filenames)) + " raw covers...")
+
+                        # If we have more than one cover, get rid of the 001 fallbacks.  This may need to be updated to run through twice to ensure we havent hit
+                        # a situation where we have more than one 001 fallbacks and no 000 covers
+                        if len(cover_filenames) > 1:
+                            zero_found = False
+                            # Look for any covers whos page number matches the zero pattern
+                            for cover_filename in sorted(cover_filenames):
+                                check_filename = cover_filename.replace(common_name,"")
+                                filename,file_extension = os.path.splitext(check_filename)
+                                if altcheck.search(check_filename) is not None:
+                                    zero_found = True
+
+                            if zero_found:
+                                # since we have a zero, let's strip out the 01 001 pages
+                                for cover_filename in sorted(cover_filenames):
+                                    check_filename = cover_filename.replace(common_name,"")
+                                    filename,file_extension = os.path.splitext(check_filename)
+                                    if not onecheck.search(check_filename):
+                                        final_cover_filenames.append(cover_filename)
+                            else:
+                                # No zeros found, so we will just accept the 01 as the fallback
+                                for cover_filename in sorted(cover_filenames):
+                                    final_cover_filenames.append(cover_filename)
+
+                        else:
+                            # Only one cover found, this would be the case for archives with no 00 named pages, so we have grabbed 01
+                            final_cover_filenames = cover_filenames
+
+                        logger.fdebug("Found " + str(len(final_cover_filenames)) + " covers...")
+
+                        if len(final_cover_filenames) > 0:
+
+                            # Final merged image created as a new image wide enough for all of the found covers
+                            final_image_path = os.path.join(mylar['CACHE_DIR'],'issue-' + issue.IssueID + '.jpg')
+                            merged_image = Image.new('RGB', (len(final_cover_filenames) * 600,922));
+                            logger.fdebug("Merging " + str(len(final_cover_filenames)) + " covers: " + str(len(final_cover_filenames) * 600) + "x" + str(922) + "...")
+
+                            # Extract each image from the archive
+                            for cover_filename in sorted(final_cover_filenames):
+                                logger.fdebug('Extracting cover: ' + cover_filename)
+                                infile = issuefile.getinfo(cover_filename)
+                                # Read the file in as a string buffer, saving us an unnecessary save
+                                img = Image.open( StringIO.StringIO( issuefile.read(infile) ))
+                                wpercent = (600/float(img.size[0]))
+                                hsize = int((float(img.size[1])*float(wpercent)))
+                                # resize to the max used on the site, 600 px wide
+                                img = img.resize((600,hsize), Image.ANTIALIAS)
+                                if( hsize > 922 ):
+                                    img = img.crop((0,0,600,922))
+                                merged_image.paste( img, (covercount * 600,0));
+                                covercount += 1
+                        
+                            try:
+                                merged_image.save(final_image_path, "JPEG", quality=merged_quality)
+                                logger.fdebug('Issue Cover Composite for Issue #' + issue.IssueID + ' successfully saved to ' + final_image_path )
+                            except:
+                                logger.fdebug('Could not save merged image' + str(sys.exc_info()))
+
 
     else:
-        logger.fdebug("Well, you didn't enable Cover View...")
+        logger.fdebug("Well, you didn't enable Cover Composition...")
 
 
 
